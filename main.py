03c99ad4ced86b3746cd65db5cb8428ca08d71db
somethingx64 <zhulixinga@163.com>
2016-03-09 21:13:18 +0800

    add some comments

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ import weibologin
 import weibospider
 if __name__ == '__main__':#global main  
     weiboSpider=weibospider.WeiboSpider()
-    weiboSpider.SpiderLogin('azhulixing@126.com','654a543')
+    weiboSpider.SpiderLogin('yourusername','yourcode')
     #weiboSpider.SpiderSpide('http://weibo.com/u/3131400905',10000)#spide 10000 users
     weiboSpider.SpiderSpide('3131400905',10000)
